f63cfd33f57bbe4f21394906d32d43dd6e175a3f
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-09 18:55:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,16 +5,6 @@ def find_even_values(src)
   src = ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] )
   
   row_index = 0
-  while row_index < src.count do
-    while element_index < src[row_index].count
-    do 
-      if src[row_index][element_index] even?
-        p src[row_index][element_index]
-      end
-      element_index += 1 
-    end
-    end
-    
-    row_index += 1 
+  element_index = 0 
   
-  end
+ 
